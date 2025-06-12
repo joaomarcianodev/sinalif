830f0d8296c252bc7faef936b2ed8cc96a9b14cd
João Augusto Marciano Silva <68918598+JoaoAugusto2020@users.noreply.github.com>
2025-06-12 15:21:19 -0300

    Delete sinalif-srv1/src/main/java/prova/sinalif directory

--- sinalif-srv1/src/main/java/prova/sinalif/repositories/LogReproducaoRepository.java
+++
@@ -1,7 +0,0 @@
-package prova.sinalif.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import prova.sinalif.models.LogReproducao;
-
-public interface LogReproducaoRepository extends JpaRepository<LogReproducao, Long> {
-}
